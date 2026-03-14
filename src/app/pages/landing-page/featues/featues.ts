cd5457da61d7af4d16cbee68ea541ad513dd18f7
HP <rajifatimazakkiya@gmail.com>
2026-03-14 11:51:43 +0100

    first commit

---
+++ src/app/pages/landing-page/featues/featues.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-featues',
+  imports: [],
+  templateUrl: './featues.html',
+  styleUrl: './featues.css',
+})
+export class Featues {
+
+}
