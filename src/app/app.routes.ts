39312f802558ea395c874280558fa7a3e0f2d858
immaezinne <immaculatei183@gmail.com>
2026-03-16 13:15:46 +0100

    created login page structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/auth/login/login';
+import { Register } from './pages/auth/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: Register },
+];
