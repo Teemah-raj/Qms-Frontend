50a714ebe99a2d8f65845031033c565b97328227
immaezinne <immaculatei183@gmail.com>
2026-03-23 10:02:57 +0100

    Created Login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,5 @@ import { LoginComponent } from './pages/auth/login/login';
 import { Register } from './pages/auth/register/register';
 
 export const routes: Routes = [
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: Register },
+    
 ];
