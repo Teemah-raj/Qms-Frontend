779872ad5b4cafa3d1a1f27d6294e375e2884e0a
Mandie <mandyokwyogbodo27@gmail.com>
2026-04-09 17:08:10 +0100

    changed from 'signup' to 'register'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/auth/login/login';
+import { LandingPage } from './pages/landing-page/landing-page';
+import { Register } from './pages/auth/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: LandingPage},
+  { path: 'register', component: Register},
+  { path: 'login', component: Login},
+];
--- src/app/pages/auth/register/register.ts
+++ src/app/pages/auth/register/register.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule, TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+
+  imports: [CommonModule, TitleCasePipe],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
+  currentStep: number = 1; 
+  selectedRole: string = 'customer'; // The data variable
 
+  // The function (the "doer")
+  selectRole(role: string) {
+    this.selectedRole = role;
+    this.currentStep = 2;
+  }
+
+  toggleRole(role: string) {
+    this.selectedRole = role;
+  }
 }
